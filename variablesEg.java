6feb4e8a181cb0a38247e52e0181e858239e0190
AparnaSabu123 <140134641+AparnaSabu123@users.noreply.github.com>
2023-07-22 08:48:57 +0530

    First commit

---
+++ variablesEg.java
@@ -0,0 +1,12 @@
+class VariablesEg
+{
+static char c='S';
+String s="Hello";
+public static void main(String args[])
+{
+int a=10;//local variable
+System.out.println(a);
+System.out.println(c);
+System.out.println(s);
+}
+}
